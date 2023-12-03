454e39be886845e2cca2da831a7c3e7e2cf5ffec
Ayan <ayansingal676@gmail.com>
2023-12-02 21:08:48 -0500

    a

---
+++ wds-overhaul/speech-to-text.js
@@ -0,0 +1,17 @@
+const GetSpeech = () => {
+    console.log("clicked microphone");
+    const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
+   
+    let recognition = new SpeechRecognition();recognition.onstart = () => {
+            console.log("starting listening, speak in microphone");
+        }
+        recognition.onspeechend = () => {
+            console.log("stopped listening");
+            recognition.stop();
+        }
+        recognition.onresult = (result) => {
+            console.log(result.results[0][0].transcript);
+         }
+     
+         recognition.start();
+}
