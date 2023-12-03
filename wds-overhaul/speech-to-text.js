50d983adfa6c1df45c4c205bd0e53c0045428ec5
Ayan <ayansingal676@gmail.com>
2023-12-02 21:49:21 -0500

    speech-recognition

--- wds-overhaul/speech-to-text.js
+++ wds-overhaul/speech-to-text.js
@@ -1,17 +1,41 @@
-const GetSpeech = () => {
-    console.log("clicked microphone");
-    const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
-   
-    let recognition = new SpeechRecognition();recognition.onstart = () => {
-            console.log("starting listening, speak in microphone");
-        }
-        recognition.onspeechend = () => {
-            console.log("stopped listening");
-            recognition.stop();
-        }
-        recognition.onresult = (result) => {
-            console.log(result.results[0][0].transcript);
-         }
-     
-         recognition.start();
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const startSpeechRecognitionBtn = document.getElementById('startSpeechRecognition');
+    const editor = document.getElementById('editor');
+    const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+    let isRecognitionOn = false;
+    let currentContent = "";
+  
+    recognition.continuous = false;
+    recognition.interimResults = false;
+    recognition.lang = 'en-US';
+  
+    startSpeechRecognitionBtn.addEventListener('click', function () {
+      if (!isRecognitionOn) {
+        recognition.start();
+        isRecognitionOn = true;
+        startSpeechRecognitionBtn.textContent = 'Stop Speech Recognition';
+      } else {
+        recognition.stop();
+        isRecognitionOn = false;
+        startSpeechRecognitionBtn.textContent = 'Start Speech Recognition';
+      }
+    });
+
+    recognition.onresult = function (event) {
+      const transcript = event.results[0][0].transcript;
+      currentContent = transcript;
+      const cursorPosition = quill.getSelection();
+      quill.insertText(quill.getLength(), `${currentContent}`, 'user');
+    };
+  
+    recognition.onend = function () {
+      if (isRecognitionOn) {
+        recognition.start(); // Restart recognition after it ends
+      }
+    };
+  
+    recognition.onerror = function (event) {
+      console.error('Speech recognition error:', event.error);
+      // Handle errors as needed
+    };
+});
